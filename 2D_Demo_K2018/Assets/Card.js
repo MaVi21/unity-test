de1355d1b2270797e2bb191d257ed07ff87d7489
MaVi21 <mattiviitala70@gmail.com>
2018-01-29 12:46:39 +0200

    2D Demo 1

---
+++ 2D_Demo_K2018/Assets/Card.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+var fgSprite : Sprite;
+var bgSprite : Sprite;
+var locked : boolean = false;
+
+function Start () {
+	
+}
+
+function ShowForeground () {
+
+	GetComponent(SpriteRenderer).sprite = fgSprite;
+}
+
+function ShowBackground () {
+
+	GetComponent(SpriteRenderer).sprite = bgSprite;
+}
