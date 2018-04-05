c1b7299e0aa36e1d4d20cc1fc523e5c47dc08d97
MaVi21 <mattiviitala70@gmail.com>
2018-04-05 16:36:35 +0300

    Kempele - environment
    Audio, particle effects, trees, terrain

---
+++ Kempele_1/Assets/Scripts/AmbientAudioPlayer.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+var audioClips : AudioClip[];
+
+function Start () {
+
+	//Invoke("PlayAudio", Random.Range(3, 10) );
+	PlayAudio();
+}
+
+function PlayAudio () {
+
+	GetComponent.<AudioSource>().clip = audioClips[Random.Range(0, audioClips.length)];
+	GetComponent.<AudioSource>().Play();
+
+	Invoke("PlayAudio", GetComponent.<AudioSource>().clip.length);
+}
---
+++ Kempele_1/Assets/Scripts/RandomAudioPlayer1.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+var audioClips : AudioClip[];
+
+function Start () {
+
+	Invoke("PlayAudio", Random.Range(4, 15) );
+
+}
+
+function PlayAudio () {
+
+	GetComponent.<AudioSource>().clip = audioClips[Random.Range(0, audioClips.length)];
+	GetComponent.<AudioSource>().Play();
+
+	Invoke("PlayAudio", Random.Range(GetComponent.<AudioSource>().clip.length + 4, GetComponent.<AudioSource>().clip.length + 14) );
+}
