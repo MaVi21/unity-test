9d5792e001cb89bd5065a14fb7cc4045f7f081dd
MaVi21 <mattiviitala70@gmail.com>
2018-04-06 12:11:04 +0300

    Snowball throw
    shooting action added

---
+++ Kempele_1/Assets/Bullet.js
@@ -0,0 +1,42 @@
+﻿//class Bullet extends MonoBehaviour
+
+#pragma strict
+
+var rb : Rigidbody;
+var power : int = 5;
+//public int power = 5;
+//var audioClip : AudioClip;
+
+function Start () {
+
+	//GetComponent.<AudioSource>().clip = audioClip;
+	//print("Start");
+	rb = GetComponent.<Rigidbody>();
+	rb.AddForce(transform.forward * power, ForceMode.Impulse);
+	//destroy the bullet after five seconds
+	Destroy (gameObject, 5);
+}
+
+//this callback function will be called, when the bullet collides with another collider
+function OnCollisionEnter(collision: Collision) {
+
+	print("collision");
+
+	//detect if the bullet collided with a target
+	if(collision.gameObject.tag == "Target"){
+
+		print("target hit");
+		//find a gameobject by it's name
+		var c = GameObject.Find("Main Camera");
+		//look for function DisplayScore from all the script components of main camera
+		c.SendMessage("DisplayScore");
+	}
+
+	Destroy(gameObject);
+
+}
+
+function Update () {
+
+	//print("Update");
+}
---
+++ Kempele_1/Assets/Game.js
@@ -0,0 +1,44 @@
+﻿#pragma strict
+
+//public variables will appear in Unity editor as editable fields
+var hSpeed : int = 10;
+var vSpeed : int = 10;
+var bullet : Rigidbody;
+//var score : int = 0;
+//var scoreText : UnityEngine.UI.Text;
+
+
+function Start () {
+
+	
+}
+
+function DisplayScore(){
+
+	//score = score + 10;
+	//scoreText.text = "Score: " + score;
+
+}
+
+function Update () {
+
+	//read horizontal input, assign to a variable
+	//var h : float = Input.GetAxis("Horizontal") * Time.deltaTime * hSpeed;
+	//refer to the transform component of the container object (Camera)
+	//use Time class to achieve constant game play speed
+	//transform.Translate (h, 0, 0);
+
+	//aim with the "gun" by rotating the camera
+	//var v : float = Input.GetAxis("Vertical") * Time.deltaTime * vSpeed;
+	//transform.Rotate(v, 0, 0);
+
+	if (Input.GetButtonDown("Fire1")) {
+
+		var pos : Vector3 = transform.position;
+		pos.y += 1;
+		pos.x += 0.45;
+		print("fire");
+		//create an instance of a prefab (bullet)
+		var b : Rigidbody = Instantiate(bullet, pos, transform.rotation);
+	}
+}
